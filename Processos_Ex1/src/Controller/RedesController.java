02160dbe10aaaed3ad9cdba0ccac0a353af70244
Luigi <luigilucas95@gmail.com>
2023-03-03 14:30:40 -0300

    Initial commit

--- Processos_Ex1/src/Controller/RedesController.java
+++ Processos_Ex1/src/Controller/RedesController.java
@@ -11,35 +11,87 @@ public class RedesController {
 		super();
 	}
 
-	public String os() {
+	private static String os() {
 
 		String os = System.getProperty("os.name");
 		return os;
 
 	}
 
-	public void ip(String process) {
-		try {
-			Process p = Runtime.getRuntime().exec(process);
-			InputStream fluxo = p.getInputStream();
-			InputStreamReader leitor = new InputStreamReader(fluxo);
-			BufferedReader buffer = new BufferedReader(leitor);
-			String linha = buffer.readLine();
-			while (linha != null) {
-				if (linha.contains("IPv4")) {
-					System.out.println(linha);
+	public void ip() {
+		String os = Controller.RedesController.os();
+		String process = " ";
+		if (os.contains("Windows")) {
+			process = "ipconfig";
+			try {
+				Process p = Runtime.getRuntime().exec(process);
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					if (linha.contains("IPv4")) {
+						System.out.println(linha);
+					}
+					linha = buffer.readLine();
 				}
-				linha = buffer.readLine();
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			buffer.close();
-			leitor.close();
-			fluxo.close();
-		} catch (IOException e) {
-			e.printStackTrace();
+		} else if (os.contains("Linux")) {
+			process = "ifconfig";
+			try {
+				Process p = Runtime.getRuntime().exec(process);
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					if (linha.contains("netmask")) {
+						System.out.println(linha);
+					}
+					linha = buffer.readLine();
+				}
+				buffer.close();
+				leitor.close();
+				fluxo.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
-	public void ping(String process) {
+	public void ping() {
+		String os = Controller.RedesController.os();
+		String process = " ";
+		if (os.contains("Windows")) {
+			process = "PING -4 -n 10 www.google.com.br";
+			try {
+				Process p = Runtime.getRuntime().exec(process);
+				InputStream fluxo = p.getInputStream();
+				InputStreamReader leitor = new InputStreamReader(fluxo);
+				BufferedReader buffer = new BufferedReader(leitor);
+				String linha = buffer.readLine();
+				while (linha != null) {
+					if (linha.contains("M�dia")) {
+						String VT[] = linha.split(",");
+						for (String media : VT) {
+							if (media.contains("M�dia")) {
+								System.out.println(media);
+							}
+						}
+					}
+					linha = buffer.readLine();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else if (os.contains("Linux")) {
+			process = "ping -4 -c 10 www.google.com.br";
+		}
 		try {
 			Process p = Runtime.getRuntime().exec(process);
 			InputStream fluxo = p.getInputStream();
@@ -47,13 +99,11 @@ public class RedesController {
 			BufferedReader buffer = new BufferedReader(leitor);
 			String linha = buffer.readLine();
 			while (linha != null) {
-				if (linha.contains("M�dia")) {
-					String VT[] = linha.split(",");
-					for (String media : VT) {
-						if (media.contains("M�dia")) {
-							System.out.println(media);
-						}
-					}
+				if (linha.contains("rtt")) {
+					String VT[] = linha.split("/");
+					String media = VT[4];
+					System.out.println("Tempo médio do ping: " + media + " ms");
+
 				}
 				linha = buffer.readLine();
 			}
--- Processos_Ex1/src/View/Main.java
+++ Processos_Ex1/src/View/Main.java
@@ -10,32 +10,16 @@ public class Main {
 
 		RedesController Rc = new RedesController();
 
-		String os = Rc.os();
-
-		System.out.println("Sistema Operacional: " + os);
-
 		int opc = 0;
 		while (opc != 9) {
 			opc = Integer
 					.parseInt(JOptionPane.showInputDialog("1 - Verificar ip \n 2 - Verificar ping \n 9 - Finalizar"));
 			switch (opc) {
 			case 1:
-				if (os.contains("Windows")) {
-					String process = "ipconfig";
-					Rc.ip(process);
-				} else {
-					String process = "ifconfig";
-					Rc.ip(process);
-				}
+				Rc.ip();
 				break;
 			case 2:
-				if (os.contains("Windows")) {
-					String process = "PING -4 -n 10 www.google.com.br";
-					Rc.ping(process);
-				} else {
-					String process = "PING -4 -c 10 www.google.com.br";
-					Rc.ping(process);
-				}
+				Rc.ping();
 				break;
 			case 9:
 				System.out.println("Programa finalizado");
